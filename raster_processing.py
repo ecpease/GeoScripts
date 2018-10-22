6cac7b8bfc77065ffbfaf19ecb92a79602cbcc32
Emily Pease <epease@usgs.gov>
2018-10-22 17:53:13 -0500

    cleaned code, changed nothing

--- raster_processing.py
+++ raster_processing.py
@@ -1,11 +1,9 @@
-import geopandas as gpd
+﻿import geopandas as gpd
 import pandas as pd
 from shapely.geometry import Point
 import os
 
-
-# Mosaic Conditioned Rasters and assign projection
-
+# Read in and mosaic all rasters from a folder
 dirpath = os.path.join(')
 out_fp = os.path.join('path', 'to', 'file_output.ext')
 search_criteria = r"*.bil" # delete any xmls in the folder or it wont work, sorry
@@ -21,9 +19,11 @@ print(src_files_to_mosaic) # If get error: Memory Error, delecte any xmls or .ov
 mosaic, out_trans = merge(src_files_to_mosaic)
 out_meta = src.meta.copy()
 
+#Assign raster projection
 proj = '﻿Proj4: +proj=longlat +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +no_defs'  # NAD 83
 out_meta.update({"driver": "GTiff", "height": mosaic.shape[1], "width": mosaic.shape[2], "transform": out_trans, "crs":
                  proj})
+# Write to file
 print("Writing file...")
 with rasterio.open(out_fp, "w", **out_meta) as dest:
     kwargs = src.meta
@@ -32,13 +32,3 @@ with rasterio.open(out_fp, "w", **out_meta) as dest:
         dtype=rasterio.uint32
     )
     dest.write(mosaic)
-
-
-# Re-Project Conditioned Raster to NAD 83 Albers Equal Area
-
-# print("Reprojecting raster to NAD83 Albers Equal Area...")
-# os.chdir("D:\\path\\to\\folder")
-# os.system('gdalwarp input.tif output_reprojected.tif -t_srs "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 '
-#           '+x_0=0 +y_0=0 +ellps=GRS9- +towgs84=0,0,0,0,0,0,0 +units=m _no_defs"') # one way to do it
-# os.system('gdalwarp -t_srs "+proj=longlat +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +no_defs" -overwrite acc_test.tif flacc.tif') # another way to do it
-# os.system('gdal_translate -of "GTiff" acc.ascii acc_test.tif') # translate from one type of file to another
--- ws_poly_processing.py
+++ ws_poly_processing.py
@@ -1,3 +1,17 @@
+"""
+Written by Emily Pease
+
+Script to open a layer (shapefile) and determine how many features are present.  This is in the
+context of watershed delineation.  It creates three columns called Site_ID, Site_Name,
+and Area_sq_mi in addition to deleting any extra rows present in the geodataframe
+(attribute table) that the analyst would not want included.
+
+Often times, during watershed delineation, ESRI will create stand-alone raster cells as
+part of the watershed.  This script deletes all of those.  
+
+"""
+
+
 import geopandas as gpd
 import os
 
@@ -14,5 +28,5 @@ if len(ws) > 1:
     print(ws.touches(ws))
     ws.to_file('ws_delrows.shp')
 elif len(ws) == 1:
-    print("You're good to go, Em!")
-    print("Thanks, Em :)")
+    print("You're good to go!")
+    print("Thanks :)")
